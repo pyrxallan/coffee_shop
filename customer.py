769642861f06c701f27216a65b907378122225ff
Allan Ratemo <allanisratemo@gmail.com>
2025-11-23 08:52:30 +0300

    Initial

---
+++ customer.py
@@ -0,0 +1,13 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 15):
+            raise ValueError("Name must be a string between 1 and 15 characters.")
+        self._name = name
